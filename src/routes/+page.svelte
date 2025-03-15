b74f07cf3c9b450529fc7d1d611b8c313b42d830
kolown2007 <kolown@gmail.com>
2025-03-15 12:55:24 +0800

    Remove Firebase initialization code from the main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,5 @@
 
-<script lang="ts">
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAKlJpVAsafQaJ6c5DSc2NWf9MsxZ4nrOo",
-  projectId: "internetpavilion2024",
-  appId: "1:205468613342:web:86e97e4a64b0b0212e604f",
-  measurementId: "G-R3H14TCQ9C"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-
-</script>
 
 <div class="min-h-screen bg-black flex items-center justify-center">
     <div class="text-center">
